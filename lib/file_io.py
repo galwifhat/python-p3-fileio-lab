dabe052f2518b615d7872a5a2abfe48041316c42
Ruth <cirurue@gmail.com>
2025-05-25 23:53:12 +0300

    read, appended amd wrote files

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode='w', encoding='utf-8') as file:
+        file.write(file_content)
+    return file_content
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode='a', encoding='utf-8') as file:
+        file.write(append_content)
+    return append_content
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', mode='r', encoding='utf-8') as file:
+        return file.read()
+
+
+
+print(write_file(file_name="logfile", file_content="Log 1: 5 bananas added" ))
+print(append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted"))
+print(read_file(file_name="logfile"))
